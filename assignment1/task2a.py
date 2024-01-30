a0037767cffbe61540fe0c5e5a7827149e06dfbf
soricm <marijan.soricx@gmail.com>
2024-01-30 20:54:40 +0100

    30_janv

--- assignment1/task2a.py
+++ assignment1/task2a.py
@@ -13,6 +13,10 @@ def pre_process_images(X: np.ndarray):
     assert X.shape[1] == 784,\
         f"X.shape[1]: {X.shape[1]}, should be 784"
     # TODO implement this function (Task 2a)
+    batch_size = X.shape[0]
+    minX, maxX = np.min(X), np.max(X)
+    X = (X - minX)*2/(maxX - minX) - 1
+    X = np.hstack((X,np.ones((batch_size,1))))
     return X
 
 
@@ -27,14 +31,15 @@ def cross_entropy_loss(targets: np.ndarray, outputs: np.ndarray) -> float:
     # TODO implement this function (Task 2a)
     assert targets.shape == outputs.shape,\
         f"Targets shape: {targets.shape}, outputs: {outputs.shape}"
-    return 0
+    
+    return -np.mean(targets*np.log(outputs)+(1-targets)*np.log(1-outputs))
 
 
 class BinaryModel:
 
     def __init__(self):
         # Define number of input nodes
-        self.I = None
+        self.I = 785
         self.w = np.zeros((self.I, 1))
         self.grad = None
 
@@ -46,7 +51,9 @@ class BinaryModel:
             y: output of model with shape [batch size, 1]
         """
         # TODO implement this function (Task 2a)
-        return None
+        print(self.w.shape, X.shape)
+        y = 1/(1+np.exp(-self.w.T * X))
+        return y
 
     def backward(self, X: np.ndarray, outputs: np.ndarray, targets: np.ndarray) -> None:
         """
@@ -62,6 +69,8 @@ class BinaryModel:
         self.grad = np.zeros_like(self.w)
         assert self.grad.shape == self.w.shape,\
             f"Grad shape: {self.grad.shape}, w: {self.w.shape}"
+        
+        self.grad = -(targets - outputs) @ X
 
     def zero_grad(self) -> None:
         self.grad = None
