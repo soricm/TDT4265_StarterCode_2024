1f6b3fea4894606a7906afee45ea36a767ee59cb
soricm <marijan.soricx@gmail.com>
2024-03-04 17:08:47 +0100

    task4

--- assignment3/dataloaders.py
+++ assignment3/dataloaders.py
@@ -81,3 +81,94 @@ def load_cifar10(batch_size: int, validation_fraction: float = 0.1
                                                   num_workers=2)
 
     return dataloader_train, dataloader_val, dataloader_test
+
+
+
+    transform_train = transforms.Compose([
+        # New
+        transforms.Resize((32,32)),
+        #transforms.Resize((31, 31)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ColorJitter(brightness=0.5),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomGrayscale(p=0.2),
+        
+        # Old transformations
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+        
+## For task 4
+
+def load_cifar10_ResNes(batch_size: int, validation_fraction: float = 0.1
+                 ) -> typing.List[torch.utils.data.DataLoader]:
+    # Note that transform train will apply the same transform for
+    # validation!
+    transform_train = transforms.Compose([
+        transforms.Resize((112,112)),
+        transforms.RandomHorizontalFlip(p=0.5),
+        transforms.ColorJitter(brightness=0.5),
+        transforms.RandomRotation(degrees=15),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.GaussianBlur(3, sigma=(0.1, 2.0))
+        
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    transform_validation = transforms.Compose([
+        transforms.Resize((112,112)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.Resize((112,112)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+        
+    ])
+    data_train = datasets.CIFAR10(get_data_dir(),
+                                  train=True,
+                                  download=True,
+                                  transform=transform_train)
+    # ----------------- ----------------- ----------------- ----------------- #
+    # separate dataset for validation set
+    data_validation = datasets.CIFAR10(get_data_dir(),
+                                     train=True,
+                                     download=True,
+                                     transform=transform_validation)
+    # ----------------- ----------------- ----------------- ----------------- #
+    data_test = datasets.CIFAR10(get_data_dir(),
+                                 train=False,
+                                 download=True,
+                                 transform=transform_test)
+
+    indices = list(range(len(data_train)))
+    split_idx = int(np.floor(validation_fraction * len(data_train)))
+
+    val_indices = np.random.choice(indices, size=split_idx, replace=False)
+    train_indices = list(set(indices) - set(val_indices))
+
+    train_sampler = SubsetRandomSampler(train_indices)
+    validation_sampler = SubsetRandomSampler(val_indices)
+
+    dataloader_train = torch.utils.data.DataLoader(data_train,
+                                                   sampler=train_sampler,
+                                                   batch_size=batch_size,
+                                                   num_workers=2,
+                                                   drop_last=True)
+
+    dataloader_val = torch.utils.data.DataLoader(data_validation,
+                                                 sampler=validation_sampler,
+                                                 batch_size=batch_size,
+                                                 num_workers=2)
+
+    dataloader_test = torch.utils.data.DataLoader(data_test,
+                                                  batch_size=batch_size,
+                                                  shuffle=False,
+                                                  num_workers=2)
+
+    return dataloader_train, dataloader_val, dataloader_test
